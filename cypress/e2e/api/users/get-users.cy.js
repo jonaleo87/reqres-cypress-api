3ccc945048b1a2910fc86a16703a2f414ddd11da
jonaleo87 <jonaleo87@gmail.com>
2025-01-24 09:14:41 -0300

    Finalizacion de TC get-user.cy.js

--- cypress/e2e/api/users/get-users.cy.js
+++ cypress/e2e/api/users/get-users.cy.js
@@ -1,12 +1,10 @@
 describe('GET api/users', () => {
   let response;
 
-  before(() => {
-    cy.request({
+  before(async () => {
+    response = await cy.request({
       method: 'GET',
       url: `${Cypress.env('baseUrl')}/api/users?page=2`,
-    }).then((res) => {
-      response = res; // Asignar la respuesta a la variable response
     });
   });
 
@@ -16,7 +14,8 @@ describe('GET api/users', () => {
   });
 
   it('TC02| Validar que la respuesta sea en formato JSON y que el tiempo de respuesta sea menor a 2 segundos', () => {
-    expect(response.headers['content-type']).to.eq('application/json');
+    console.log(response.headers['content-type']); // Agregar log para depuración
+    expect(response.headers['content-type']).to.include('application/json');
     expect(response.duration).to.be.lessThan(2000);
   });
 
@@ -44,6 +43,49 @@ describe('GET api/users', () => {
     expect(response.body.support).to.have.all.keys('url', 'text');
     expect(response.body.support.url).to.be.a('string');
     expect(response.body.support.text).to.be.a('string');
+  });
+
+  it('TC06| Validar consistencia en la paginación', () => {
+    expect(response.body.page).to.eq(2);
+    expect(response.body.per_page * response.body.total_pages).to.be.at.least(response.body.total);
+  });
+
+  it('TC07| Validar que haya usuarios en la página solicitada', () => {
+    expect(response.body.data).to.not.be.empty;
+  });
+
+  it('TC08| Validar que los avatares sean URLs válidas', () => {
+    response.body.data.forEach((user) => {
+      expect(user.avatar).to.match(/^https?:\/\/[\w.-]+(?:\.[\w\.-]+)+[/#?]?.*$/);
+    });
+  });
+
+  it('TC09| Validar respuesta al solicitar una página fuera del rango', async () => {
+    const emptyResponse = await cy.request({
+      method: 'GET',
+      url: `${Cypress.env('baseUrl')}/api/users?page=100`,
+    });
+    expect(emptyResponse.body.data).to.be.empty;
+  });
+
+
+  it('TC10| Validar dominio del URL de soporte', () => {
+    expect(response.body.support.url).to.contain('reqres');
+  });
+
+  it('TC11| Verificar límites de page y per_page', () => {
+    const maxPage = 50;
+    const maxPerPage = 100;
+
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('baseUrl')}/api/users?page=${maxPage}&per_page=${maxPerPage}`,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.page).to.eq(maxPage);
+      expect(response.body.per_page).to.eq(maxPerPage);
+      expect(response.body.data).to.be.an('array');
+    });
   });
 
 });

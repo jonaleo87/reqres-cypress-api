fac9250ee362ef398cfd5f6ff8f440b8f5723951
jonaleo87 <jonaleo87@gmail.com>
2025-01-27 18:09:16 -0300

    creacion test manejo de errores login y register

--- cypress/e2e/api/auth/post-register-unsuccessful.cy.js
+++ cypress/e2e/api/auth/post-register-unsuccessful.cy.js
@@ -23,11 +23,20 @@ describe('POST api/register unsuccessful', () => {
         expect(response.status).to.eq(400);
     });
 
-    it('CP02| Validar que el tiempo de respuesta sea menor a 2 segundos', () => {
+    it('CP02| Validar que el mensaje de error sea correcto', () => {
+        expect(response.body).to.have.property('error');
+        expect(response.body.error).to.eq('Missing password');
+    });
+
+    it('CP03| Validar que el tiempo de respuesta sea menor a 2 segundos', () => {
         expect(response.duration).to.be.lessThan(2000);
     });
 
-    it('CP03| Validar que el body de la respuesta contenga el error esperado', () => {
-        expect(response.body).to.have.property('error').and.to.eq('Missing password');
+    it('CP04| Validar que el método del request sea POST', () => {
+        expect(response.requestHeaders).to.have.property('method', 'POST');
+    });
+
+    it('CP05| Validar que la URL del request sea correcta', () => {
+        expect(response.requestHeaders).to.have.property('url', `${Cypress.env('baseUrl')}/api/register`);
     });
 });
